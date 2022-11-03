68feb60615d1234ab79ae4ea8158a015aa936bcb
PhamDucPhuongMinh <phamducphuongminh0103@gmail.com>
2022-11-03 09:18:44 +0700

    set up project

---
+++ src/components/GlobalStyles/GlobalStyles.js
@@ -0,0 +1,9 @@
+import "./GlobalStyles.scss";
+// import classNames from "classnames/bind";
+// import styles from "./GlobalStyles.module.scss";
+
+// const cx = classNames.bind(styles);
+
+export default function GlobalStyles({ children }) {
+  return children;
+}
