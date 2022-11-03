91b83cd62e3dffccbca8e0e25ac37833b1ce1167
PhamDucPhuongMinh <phamducphuongminh0103@gmail.com>
2022-11-03 09:44:31 +0700

    Initialize firebase

---
+++ src/firebase/firebase-config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDa3d29dlq7j56eePPSPElTueojpPT99T0",
+  authDomain: "userwimesh.firebaseapp.com",
+  projectId: "userwimesh",
+  storageBucket: "userwimesh.appspot.com",
+  messagingSenderId: "439339650481",
+  appId: "1:439339650481:web:08b4792875786576244190",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Init services
+const db = getFirestore();
