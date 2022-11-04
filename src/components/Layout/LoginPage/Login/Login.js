51259c3e66da6c1705bf51e66ab1fbde78a8afad
PhamDucPhuongMinh <phamducphuongminh0103@gmail.com>
2022-11-04 08:50:01 +0700

    validate form

--- src/components/Layout/LoginPage/Login/Login.js
+++ src/components/Layout/LoginPage/Login/Login.js
@@ -1,10 +1,23 @@
 import React from "react";
 import classNames from "classnames/bind";
+import { ErrorMessage } from "@hookform/error-message";
+import { useForm } from "react-hook-form";
 import styles from "./Login.module.scss";
 
 const cx = classNames.bind(styles);
 
 export default function Login({ onChangeForm }) {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({
+    criteriaMode: "all",
+    mode: "all",
+  });
+
+  const onHandleSubmit = (data) => console.log(data);
+
   return (
     <div className={cx("login")}>
       <h4>Login</h4>
@@ -14,21 +27,64 @@ export default function Login({ onChangeForm }) {
           Create An Account
         </button>
       </p>
-      <form className={cx("login__form")}>
+      <form
+        className={cx("login__form")}
+        onSubmit={handleSubmit(onHandleSubmit)}
+      >
         <div className={cx("form-group")}>
           <p className={cx("form-group__label")}>Phone number</p>
-          <input type="number" />
-          <p className={cx("form-group__danger")}>
-            Phone number must consist of 10 digits
-          </p>
+          <input
+            type="number"
+            {...register("phone-number", {
+              required: "Phone number is required.",
+              minLength: {
+                value: 10,
+                message: "Phone number must consist of 10 digits",
+              },
+              maxLength: {
+                value: 10,
+                message: "Phone number must consist of 10 digits",
+              },
+            })}
+          />
+          <ErrorMessage
+            errors={errors}
+            name="phone-number"
+            render={({ messages }) => {
+              return messages
+                ? Object.entries(messages).map(([type, message]) => (
+                    <p key={type} className={cx("form-group__danger")}>
+                      {message}
+                    </p>
+                  ))
+                : null;
+            }}
+          />
         </div>
         <div className={cx("form-group")}>
           <p className={cx("form-group__label")}>
             <span>Password</span>
             <span className="text-primary text-link">Forgot Password ?</span>
           </p>
-          <input type="password" />
-          <p className={cx("form-group__danger")}>Password is required</p>
+          <input
+            type="password"
+            {...register("password", {
+              required: "Password is required.",
+            })}
+          />
+          <ErrorMessage
+            errors={errors}
+            name="password"
+            render={({ messages }) => {
+              return messages
+                ? Object.entries(messages).map(([type, message]) => (
+                    <p key={type} className={cx("form-group__danger")}>
+                      {message}
+                    </p>
+                  ))
+                : null;
+            }}
+          />
         </div>
         <div className={cx("form-group")}>
           <button className={`${cx("form__btn")} btn btn--black`}>Login</button>
--- src/components/Layout/LoginPage/SignUp/SignUp.js
+++ src/components/Layout/LoginPage/SignUp/SignUp.js
@@ -1,29 +1,146 @@
-import React from "react";
+import React, { useRef } from "react";
 import classNames from "classnames/bind";
+import { ErrorMessage } from "@hookform/error-message";
+import { useForm } from "react-hook-form";
 import styles from "./SignUp.module.scss";
 
 const cx = classNames.bind(styles);
 
 export default function SignUp({ onChangeForm }) {
+  const {
+    register,
+    formState: { errors },
+    watch,
+    handleSubmit,
+  } = useForm({
+    criteriaMode: "all",
+    mode: "all",
+  });
+
+  const password = useRef({});
+  password.current = watch("password", "");
+
+  const onHandleSubmit = (data) => console.log(data);
+
   return (
     <div className={cx("sign-up")}>
       <h4>Sign Up</h4>
       <p>Enter your detail to create your account</p>
-      <form className={cx("sign-up__form")}>
+      <form
+        className={cx("sign-up__form")}
+        onSubmit={handleSubmit(onHandleSubmit)}
+      >
         <div className={cx("form-group")}>
-          <input type="text" placeholder="Fullname" />
+          <input
+            placeholder="Fullname"
+            {...register("full-name", {
+              required: "Fullname is required.",
+            })}
+          />
+          <ErrorMessage
+            errors={errors}
+            name="full-name"
+            render={({ messages }) => {
+              return messages
+                ? Object.entries(messages).map(([type, message]) => (
+                    <p key={type} className={cx("form-group__danger")}>
+                      {message}
+                    </p>
+                  ))
+                : null;
+            }}
+          />
         </div>
         <div className={cx("form-group")}>
-          <input type="number" placeholder="Phone number" />
+          <input
+            placeholder="Phone number"
+            type="number"
+            {...register("phone-number", {
+              required: "Phone number is required.",
+              minLength: {
+                value: 10,
+                message: "Phone number must consist of 10 digits",
+              },
+              maxLength: {
+                value: 10,
+                message: "Phone number must consist of 10 digits",
+              },
+            })}
+          />
+          <ErrorMessage
+            errors={errors}
+            name="phone-number"
+            render={({ messages }) => {
+              return messages
+                ? Object.entries(messages).map(([type, message]) => (
+                    <p key={type} className={cx("form-group__danger")}>
+                      {message}
+                    </p>
+                  ))
+                : null;
+            }}
+          />
         </div>
         <div className={cx("form-group")}>
-          <input type="password" placeholder="Password" />
+          <input
+            placeholder="Password"
+            type="password"
+            {...register("password", {
+              required: "Password is required.",
+              pattern: {
+                value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+                message:
+                  "Minimum eight characters, at least one letter and one number.",
+              },
+            })}
+          />
+          <ErrorMessage
+            errors={errors}
+            name="password"
+            render={({ messages }) => {
+              return messages
+                ? Object.entries(messages).map(([type, message]) => (
+                    <p key={type} className={cx("form-group__danger")}>
+                      {message}
+                    </p>
+                  ))
+                : null;
+            }}
+          />
         </div>
         <div className={cx("form-group")}>
-          <input type="password" placeholder="Confirm password" />
+          <input
+            placeholder="Confirm password"
+            type="password"
+            {...register("comfirm-password", {
+              required: "Password comfirmation is required.",
+              validate: (value) =>
+                value === password.current ||
+                "The password comfirmation does not match",
+            })}
+          />
+          <ErrorMessage
+            errors={errors}
+            name="comfirm-password"
+            render={({ messages }) => {
+              return messages
+                ? Object.entries(messages).map(([type, message]) => (
+                    <p key={type} className={cx("form-group__danger")}>
+                      {message}
+                    </p>
+                  ))
+                : null;
+            }}
+          />
         </div>
         <div className={cx("form-group", "form-group--checkbox")}>
-          <input type="checkbox" id="accept-terms" />
+          <input
+            type="checkbox"
+            id="accept-terms"
+            {...register("accept-terms", {
+              required: "You must accept the terms and conditions.",
+            })}
+          />
           <label htmlFor="accept-terms">
             I Agree the{" "}
             <a
@@ -36,6 +153,19 @@ export default function SignUp({ onChangeForm }) {
             </a>
             .
           </label>
+          <ErrorMessage
+            errors={errors}
+            name="accept-terms"
+            render={({ messages }) => {
+              return messages
+                ? Object.entries(messages).map(([type, message]) => (
+                    <p key={type} className={cx("form-group__danger")}>
+                      {message}
+                    </p>
+                  ))
+                : null;
+            }}
+          />
         </div>
         <div className={cx("form-group")}>
           <button className={`${cx("form__btn")} btn btn--primary`}>
