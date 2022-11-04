c1b957e621a8edd7480881aeeed94abe014dd892
PhamDucPhuongMinh <phamducphuongminh0103@gmail.com>
2022-11-04 12:02:02 +0700

    layout homepage

---
+++ src/components/Layout/HomePage/HomePage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <>
+      <div>HomePage</div>
+      <p>Bạn đang đăng nhập bằng tài khoản 123</p>
+    </>
+  );
+}
